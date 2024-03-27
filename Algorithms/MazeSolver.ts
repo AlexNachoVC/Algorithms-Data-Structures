a0678fedb05e4b110dec206afcc850f98315e069
Alejandro <alexnachovc12@gmail.com>
2024-03-26 23:47:53 -0600

    Maze Solver (Recursive Case)

--- Algorithms/MazeSolver.ts
+++ Algorithms/MazeSolver.ts
@@ -1,4 +1,11 @@
-function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][]): boolean {
+const dir = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+];
+
+function walk(maze: string[], wall: string, curr: any /*Point*/, end: any /*Point*/, seen: boolean[][], path: string[]/*Point[]*/): boolean {
 
     // 1. Base Case
     // Off the map
@@ -15,6 +22,7 @@ function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boole
 
     // If it reached the end
     if (curr.x === end.x && curr.y === end.y) {
+        path.push(end);
         return true;
     }
 
@@ -22,9 +30,37 @@ function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boole
     if (seen[curr.y][curr.x]) {
         return false;
     }
+
+    // pre
+    seen[curr.y][curr.x] = true;
+    path.push(curr);
+
+    // recurse
+    for (let i = 0; i < dir.length; ++i) {
+        const [x,y] = dir[i]
+        if (walk(maze, wall, {
+            x: curr.x + x,
+            y: curr.y + y,
+        }, end, seen, path)) {
+            return true;
+        }
+    }
+
+    // post
+    path.pop();
+    return false;
 }
 
 
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+export default function solve(maze: string[], wall: string, start: any /*Point*/, end: any /*Point*/): string[] /*Point[]*/ {
+    const seen: boolean[][] = [];
+    const path: string[] /*Point[]*/ = [];
 
+    for (let i = 0; i < maze.length; ++i) {
+        seen.push (new Array(maze[0].length).fill(false));
+    }
+
+    walk(maze, wall, start, end, seen, path);
+
+    return path;
 }
