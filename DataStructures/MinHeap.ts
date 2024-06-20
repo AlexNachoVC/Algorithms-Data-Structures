3dbabe8e9c56d058cde4f32ff891386875d4c97d
Alejandro <alexnachovc12@gmail.com>
2024-06-20 15:32:31 -0600

    MinHeap complete: implement insert and delete

--- DataStructures/MinHeap.ts
+++ DataStructures/MinHeap.ts
@@ -8,11 +8,27 @@ export default class MinHeap {
     }
 
     insert(value: number): void {
-
+        this.data[this.length] = value;
+        this.heapifyUp(this.length);
+        this.length++;
     }
 
     delete(): number {
+        if (this.length === 0) {
+            return -1;
+        }
 
+        const out = this.data[0];
+        this.length--;
+
+        if (this.length === 0) {
+            this.data = [];
+            return out;
+        }
+
+        this.data[0] = this.data[this.length];
+        this.heapifyDown(0); 
+        return out;
     }
 
     private heapifyDown(idx: number): void {
