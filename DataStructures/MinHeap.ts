ff9f3df91c8755fc390ec8443572feb81927bd97
Alejandro <alexnachovc12@gmail.com>
2024-06-20 14:56:42 -0600

    MinHeap constructor

---
+++ DataStructures/MinHeap.ts
@@ -0,0 +1,17 @@
+export default class MinHeap {
+    public length: number;
+    private data: number[];
+
+    constructor() {
+        this.data = [];
+        this.length = 0;
+    }
+
+    insert(value: number): void {
+
+    }
+
+    delete(): number {
+
+    }
+}
