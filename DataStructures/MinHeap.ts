0316d9d4ac269f322fecb329d49098500907d6d7
Alejandro <alexnachovc12@gmail.com>
2024-06-20 15:18:29 -0600

    MinHeap: implementation of heapifyDown, and heapifyUp

--- DataStructures/MinHeap.ts
+++ DataStructures/MinHeap.ts
@@ -15,7 +15,44 @@ export default class MinHeap {
 
     }
 
-    
+    private heapifyDown(idx: number): void {
+        const lIdx = this.leftChild(idx);
+        const rIdx = this.rightChild(idx);
+
+        if (idx >= this.length || lIdx >= this.length)  {
+            return;
+        }
+
+        const lV = this.data[lIdx];
+        const rV = this.data[rIdx];
+        const v = this.data[idx];
+
+        if (lV > rV && v > rV) {
+            this.data[idx] = rV;
+            this.data[rIdx] = v;
+            this.heapifyDown(rIdx);
+        } else if (rV > lV && v > lV) {
+            this.data[idx] = lV;
+            this.data[lIdx] = v;
+            this.heapifyDown(lIdx);
+        }
+    }
+
+    private heapifyUp(idx: number): void {
+        if (idx === 0) {
+            return;
+        }
+
+        const p = this.parent(idx);
+        const parentV = this.data[p];
+        const  v = this.data[idx];
+
+        if (parentV > v) {
+            this.data[idx] = parentV;
+            this.data[p] = v;
+            this.heapifyUp(p);
+        }
+    }
 
     private parent(idx: number): number {
         return Math.floor((idx - 1) / 2);
