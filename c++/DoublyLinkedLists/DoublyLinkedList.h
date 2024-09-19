03c6749de9b8eabb7cb092310eb43459055126b4
Alejandro <alexnachovc12@gmail.com>
2024-09-19 09:54:12 -0600

    Add folder for doubly linked lists, below c++, and build the header file

---
+++ c++/DoublyLinkedLists/DoublyLinkedList.h
@@ -0,0 +1,26 @@
+#pragma once
+
+class Node {
+public:
+    int data;
+    Node *prev;
+    Node *next;
+};
+
+class DoublyLinkedList {
+private: 
+    Node *head;
+    Node *tail;
+public:
+    DoublyLinkedList();
+    ~DoublyLinkedList();
+    bool prepend(int value);
+    bool append(int value);
+    bool insertAt(int value, int position);
+    bool insertOnOrder(int value);
+    bool deleteElement(int value); 
+    bool deleteAtBeginning(int value);
+    bool deleteAtEnd(int value);
+    bool deleteAt(int value, int position);
+    void printList();
+};
