868890df433bc5e8dec35e67a8a5e0c39f7ae3ae
Alejandro <alexnachovc12@gmail.com>
2024-09-20 11:49:15 -0600

    build main.cpp file, to test all methods

---
+++ c++/DoublyLinkedLists/main.cpp
@@ -0,0 +1,38 @@
+#include "DoublyLinkedList.h"
+#include <iostream>
+
+using namespace std;
+
+#define MAX_DATA 20
+
+int main() {
+    DoublyLinkedList DLL;
+
+    for (int i = 0; i < MAX_DATA; i++) {
+        if(!DLL.append(i)) {
+            cout << "No hubo memoria\n";
+            return 0;
+        }
+    }
+
+    DLL.printListForwards();
+    DLL.append(21);
+    DLL.printListForwards();
+    DLL.prepend(0);
+    DLL.printListForwards();
+    DLL.insertAt(100, 10);
+    DLL.printListForwards();
+    DLL.insertOnOrder(5);
+    DLL.printListForwards();
+    DLL.deleteElement(100);
+    DLL.printListForwards();
+    DLL.deleteAtBeginning();
+    DLL.printListForwards();
+    DLL.deleteAtEnd();
+    DLL.printListForwards();
+    DLL.deleteAt(5);
+    DLL.printListForwards();
+    DLL.printListBackwards();
+    DLL.deleteLinkedList();
+    DLL.printListForwards();
+}
