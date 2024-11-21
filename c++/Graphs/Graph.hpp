f95f179ee09bf19059f6daad57ee5a463e1e9167
Alejandro <alexnachovc12@gmail.com>
2024-11-20 22:36:51 -0600

    Graph, full implementation done. lets try it on the D&D

--- c++/Graphs/Graph.hpp
+++ c++/Graphs/Graph.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "../DoublyLinkedLists/DoublyLinkedListTemplate.hpp"
-
+#include "../Queue/Queue.hpp"
 template <typename T>
 class Vertex {
 public:
@@ -15,6 +15,12 @@ class Graph {
 private:
     Vertex<T>* vertexes;
     unsigned int size;
+
+    void clearVisited() {
+        for (unsigned int i = 0; i < size; ++i) {
+            vertexes[i].visited = false;
+        }
+    }
 
 public:
     Graph() {
@@ -89,4 +95,142 @@ public:
             cout << endl;
         }
     }
+
+    void DFS(unsigned int vertex) {
+        vertexes[vertex].visited = true;
+        cout << vertex << " ";
+
+        DoublyLinkedList<int>::Iterator i(nullptr);
+        for (auto edge : vertexes[vertex].edges) {
+            if (!vertexes[*edge].visited) {
+                DFS(*edge);
+            }
+        }
+    }
+
+    bool BFS(unsigned int startVertex) {
+        Queue<unsigned int> queue;
+
+        if (!size || startVertex >= size) {
+            return false;
+        }
+
+        clearVisited();
+
+        cout << "BFS(" << startVertex << "): ";
+
+        if (!queue.enqueue(startVertex)) {
+            return false;
+        }
+        vertexes[startVertex].visited = true;
+
+        while (!queue.isEmpty()) {
+            unsigned int vertex = *queue.peek();
+
+            cout << vertex << " ";
+            queue.deque();
+
+            for (auto edge : vertexes[vertex].edges) {
+                if (!vertexes[edge].visited) {
+                    if (!queue.enqueue(edge)) {
+                        return false;
+                    }
+                    vertexes[edge].visited = true;
+                }
+            }
+        }
+
+        cout << "\n";
+        return true;
+    }
+    
+    bool loadFile(const string& filename) {
+		ifstream        file(filename);
+		string          line;
+		unsigned int source = 0, nSize = 0;
+
+		if (!file.is_open()) {
+			cerr << "Error opening file: " << filename << endl;
+			return false;
+		}
+		if (!getline(file, line)) {
+			cout << "No header" << endl;
+			file.close();
+			return false;
+		}
+		if (line != "Grafo") {
+			file.close();
+			return false;
+		}
+		if (!getline(file, line)) {  
+			cout << "No graph size" << endl;
+			file.close();
+			return false;
+		}
+		if (!isdigit(line)) {
+			cout << "Graph size is not numerical" << endl;
+			file.close();
+			return false;
+		}
+		nSize = stoi(line);
+		if (nSize <= 0) {
+			cout << "Incorrect graph size" << endl;
+			file.close();
+			return false;
+		}
+		deleteGraph();   
+		if (!crearGrafo(nSize)) {   
+			cout << "The graph could not be created" << endl;
+			file.close();
+			return false;
+		}
+		for (source = 0; source < size; source++) {
+			if (!getline(file, line)) {  
+				cout << "No edges" << endl;
+				file.close();
+				return false;
+			}
+			if (line.length()) {    
+				stringstream ss(line);
+				string cell;
+				while (getline(ss, cell, ' ')) {
+					int destination = 0;
+					if (!isValidNumeric(cell)) {
+						file.close();
+						return false;
+					}
+					destination = stoi(cell);
+					if (!insertEdge(source, destination)) {
+						file.close();
+						return false;
+					}
+				}
+			}
+		}
+		file.close();
+		return true;
+	}
+
+	bool saveFile(const string& filename) {
+		ofstream file;
+		if (!size) {
+			return false;
+		}
+		file.open(filename, ios::out);
+		if (!file.is_open()) {
+			cerr << "Error saving file: " << filename << endl;
+			return false;
+		}
+		file << "Grafo" << endl;    
+		file << size << endl;       
+		for (unsigned int i = 0; i < size; i++) {
+			for (auto edge : vertexes[vertex].edges) {
+				file << edge << " ";
+			}
+			file << endl;
+		}
+		file.close();
+		return true;
+	}
+
 };
